9a49b2886b519356cf3574080cb082a6345a7a0a
Brantly Callaway <brantly.callaway@uga.edu>
2025-01-10 13:30:02 +0000

    version 1.4.7

--- R/imports.R
+++ R/imports.R
@@ -1,12 +1,14 @@
-#' @title BMisc
-#'
-#' @description A set of miscellaneous helper functions
-#'
-#' @docType package
 #' @name BMisc
+#' @title BMisc
+#' @description A set of miscellaneous helper functions
+#' @keywords internal
 #' @useDynLib BMisc
 #' @importFrom Rcpp sourceCpp
 #' @import graphics
 #' @import stats
-#' @importFrom dplyr group_by mutate %>% group_map
+#' @importFrom dplyr group_by mutate %>% group_map select starts_with
+#' @importFrom tidyr pivot_wider
+#' @importFrom caret findLinearCombos
+"_PACKAGE"
+
 NULL
